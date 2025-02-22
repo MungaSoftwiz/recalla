42ff6064eb0a582725657472fd9410d520bfac95
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2025-02-22 03:55:23 +0000

    feat(layout): create component to structure the main content area of the web app

--- src/components/Layout/MainContent.js
+++ src/components/Layout/MainContent.js
@@ -1,21 +1,21 @@
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
 export const drawerWidth = 280;
 
-export const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+export const Main = styled("main", {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  flexGrow: 1,
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: 0,
+  ...(open && {
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    marginLeft: 0,
-    ...(open && {
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  })
-);
+  }),
+}));
