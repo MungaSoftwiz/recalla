d12cdf1cda6a454d920ed9cb251e66c66f303536
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2025-02-22 04:36:57 +0000

    feat(app): configure root layout with font, global styles, and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,27 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { DM_Sans } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs';
 import clsx from "clsx";
 import "./globals.css";
 
-//const inter = Inter({ subsets: ["latin"] });
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Recalla",
-    description: "An app that lets users create flashcards to streamline their study process",
+  title: "Recalla",
+  description:
+    "An app that lets users create flashcards to streamline their study process",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <ClerkProvider>
-            <body className={clsx(dmSans.className, "antialiased")}>
-              {children}
-              <Analytics />
-            </body>
-        </ClerkProvider>
-
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={clsx(dmSans.className, "antialiased")}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
