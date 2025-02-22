dd9a1648308b14e6de9715e579ec64a711c0513a
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2025-02-22 17:11:04 +0000

    style: define MainContent component with dynamic margin and scrollbar styling

--- src/components/Layout/MainContent.js
+++ src/components/Layout/MainContent.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { styled } from "@mui/material/styles";
 
 export const drawerWidth = 280;
@@ -11,6 +13,22 @@ export const Main = styled("main", {
     duration: theme.transitions.duration.leavingScreen,
   }),
   marginLeft: 0,
+  overflowY: "auto",
+  "&::-webkit-scrollbar": {
+    width: "8px",
+    background: "rgba(42,36,56,0.9)",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    background: "linear-gradient(135deg, #9C55FF 0%, #D4A5FF 100%)",
+    borderRadius: "4px",
+    "&:hover": {
+      background: "linear-gradient(135deg, #8044FF 0%, #B78FFF 100%)",
+    },
+  },
+  "&::-webkit-scrollbar-track": {
+    background: "rgba(42,36,56,0.9)",
+    borderRadius: "4px",
+  },
   ...(open && {
     marginLeft: drawerWidth,
     transition: theme.transitions.create("margin", {
